8df609322095d86546f3250fab0c2abd727d4013
shyang9711 <shyang9711@gmail.com>
2024-10-19 05:28:19 -0700

    server readded

--- src/server.js
+++ src/server.js
@@ -21,11 +21,11 @@ app.use(express.json());
 
 // PostgreSQL connection
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'allons',
-  password: 'alflt902',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 // Middleware to verify JWT token
